ef772e1ba1ab52a928cabde16ee6e34a2a24e449
TensPao <senya.atn@gmail.com>
2025-04-16 15:23:33 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -2,19 +2,31 @@
 #include <stdio.h>
 
 int main() {
-    int compteur = 5; // Vous pouvez changer la valeur tant qu'elle est < 10
+    int compteur = 6; // Doit être strictement inférieur à 10
 
     if (compteur >= 10) {
         printf("Erreur : compteur doit être strictement inférieur à 10.\n");
         return 1;
     }
 
-    // Affichage du triangle rectangle
-    for (int i = 1; i <= compteur; i++) {
-        for (int j = 1; j <= i; j++) {
+    int i = 1;
+
+    while (i <= compteur) {
+        int j = 1;
+
+        while (j <= i) {
+            // Exemple d'utilisation de if + continue
+            if (j == 0) {
+                j++;
+                continue;
+            }
+
             printf("*");
+            j++;
         }
+
         printf("\n");
+        i++;
     }
 
     return 0;
